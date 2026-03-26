ab8c1733de1ac1aca2d2f7b31cc71c053b439b3c
tuanpv62 <phantuan.tech@gmail.com>
2026-03-26 13:36:30 +0700

    fix change api auth.api

--- client/src/api/auth.api.js
+++ client/src/api/auth.api.js
@@ -2,24 +2,24 @@ import api from "./axios";
 
 export const authAPI = {
   signin: async (credentials) => {
-    const res = await api.post("/auth/signin", credentials);
+    const res = await api.post("/api/v1/auth/signin", credentials);
     return res.data.data;
   },
   signup: async (credentials) => {
-    const res = await api.post("/auth/signup", credentials);
+    const res = await api.post("/api/v1/auth/signup", credentials);
     return res.data.data;
   },
   signout: async () => {
-    await api.post("/auth/signout");
+    await api.post("/api/v1/auth/signout");
   },
 
   refreshToken: async () => {
-    const res = await api.post("/auth/refresh-token");
+    const res = await api.post("/api/v1/auth/refresh-token");
     return res.data.data;
   },
 
   getMe: async () => {
-    const res = await api.get("/auth/me");
+    const res = await api.get("/api/v1/auth/me");
     return res.data.data;
   },
 };
